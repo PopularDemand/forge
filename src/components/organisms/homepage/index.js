7bda65d15666597a9e3f07685d5cc00e58c7ab6f
Alexa <alexa.anderson@outlook.com>
2017-07-22 23:59:06 -0500

    add under construction

--- src/components/organisms/homepage/index.js
+++ src/components/organisms/homepage/index.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Formio } from 'react-formio';
 import './homepage.css';
 
-class Homepage extends Component {
-  render() {
-    return (
-      <div className="homepage">
-        this is the homepage
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="jumbotron hero">
+        <h1 className="hero-text">
+          Begin your journey
+        </h1>
+        <button className="btn btn-lg primary cta-btn disabled">Under Construction</button>
+
+        <div className="form-wrapper">
+          <h3 className="form-header">Claim your name</h3>
+          <p>While you're here, claim your personal nickname!</p>
+          <Formio src="https://exrrohbiimmsewq.form.io/mailing"></Formio>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Homepage;
