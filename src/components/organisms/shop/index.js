39439942d8cc032d82c84a8966780d9d33b36928
Alexa <alexa.anderson@outlook.com>
2017-07-27 19:50:37 -0500

    Initial shop page

--- src/components/organisms/shop/index.js
+++ src/components/organisms/shop/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { chunk } from 'lodash';
 import { compose, defaultProps } from 'recompose';
 
 import './shop.css';
+import products from './products.tmp.json';
 
 export function Shop({ header }) {
   return (
@@ -17,6 +19,10 @@ export function Shop({ header }) {
         <div className="row">
           <div className="col-sm-3 hide-xs">
             <nav className="side-nav">
+              <div className="side-nav-header">
+                <h4>Categories</h4>
+              </div>
+              <Link to="#" className="side-nav-link">Most Popular</Link>
               <Link to="#" className="side-nav-link">Apparel</Link>
               <Link to="#" className="side-nav-link">Accessories</Link>
               <Link to="#" className="side-nav-link">Household</Link>
@@ -25,9 +31,25 @@ export function Shop({ header }) {
           </div>
           
           <div className="col-sm-9">
-            <div className="row">
-              <h4>Most Popular</h4>
-              <input type="text"/>
+            <div className="row grid-header">
+              <h3 className="title">Most Popular</h3>
+              <input className="search" type="text" placeholder="Search" />
+            </div>
+            <div className="row product-grid">
+              {products.length && chunk(products, 3).map((products) => (
+                <div className="product-row" key={products[0].id}>
+                  {products.map((product) => (
+                    <div className="col-sm-4" key={product.id}>
+                      <div className="product-card">
+                        <div className="name">{product.name}</div>
+                        <img src={product.image} alt="product"/>
+                        <div className="description">{product.description}</div>
+                        <div className="price">{product.price}</div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
             </div>
           </div>
         </div>
