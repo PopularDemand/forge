da76aeec7b82295c7a764c983785ec2130cd6830
Alexa <alexa.anderson@outlook.com>
2017-08-26 21:10:21 -0500

    Better layout and cards

--- src/components/decorators/withShopData.js
+++ src/components/decorators/withShopData.js
@@ -1,11 +1,9 @@
 import { compose, lifecycle } from 'recompose';
-import { getProducts } from '../../lib/shop';
 
 const enhance = compose(
   lifecycle({
     componentWillMount() {
       this.props.loadProducts()
-      .then(console.log)
     }
   })
 );
